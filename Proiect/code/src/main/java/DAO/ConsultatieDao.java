72e852aa8586bd291d4a0cb4b75b3fad9d47648f
Gabriel_Sava <71670370+Indius23@users.noreply.github.com>
2025-03-05 17:05:15 +0200

    Update ConsultatieDao.java

--- Proiect/code/src/main/java/DAO/ConsultatieDao.java
+++ Proiect/code/src/main/java/DAO/ConsultatieDao.java
@@ -1,8 +1,4 @@
-    /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DAO;
 
 import java.util.List;
@@ -11,10 +7,6 @@ import java.util.Date;
 import pojo.Medici;
 import pojo.Pacienti;
 
-/**
- *
- * @author vali
- */
 public interface ConsultatieDao {
     public void adaugaConsultatie (Consultatie consultatie);
     public List<Consultatie> afiseazaConsultatie();
