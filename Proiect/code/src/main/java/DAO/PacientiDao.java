2c4b1964ed50abeb449dacfc1efeb073811a30b2
Gabriel_Sava <71670370+Indius23@users.noreply.github.com>
2025-03-05 17:05:37 +0200

    Update PacientiDao.java

--- Proiect/code/src/main/java/DAO/PacientiDao.java
+++ Proiect/code/src/main/java/DAO/PacientiDao.java
@@ -1,17 +1,9 @@
-    /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DAO;
 
 import java.util.List;
 import pojo.Pacienti;
 
-/**
- *
- * @author vali
- */
 public interface PacientiDao {
     public void adaugaPacienti (Pacienti pacient);
     public List<Pacienti> afiseazaPacienti();
